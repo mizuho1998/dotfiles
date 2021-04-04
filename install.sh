ad9857f500d5704351464d4ce2f5f471cffbdcc3
mizuho1998 <moc.liam0000@gmail.com>
2021-04-05 00:34:18 +0900

    feat: install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# install zshrc
-sudo apt install zsh
-chsh -s $(which zsh)
+if [ "$(uname)" == 'Darwin' ]; then
+    echo 'Mac'
+    ./brew/install.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    echo 'Linux'
+    # Linuxディストリビューションごとの設定
+    if [ -f "/etc/os-release" ]; then
+        read line </etc/os-release && eval ${line}
+        case ${NAME} in
+        "CentOS Linux") ;;
+        "Ubuntu") ;;
+            ./ubuntu/install.sh
+        *) ;;
+        esac
+    fi
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
 
 # install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-if [ "$(uname)" == 'Darwin' ]; then
-    # OS='Mac'
-    echo 'Mac'
-    # homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    # OS='Linux'
-    echo 'Linux'
-else
-    echo "Your platform ($(uname -a)) is not supported."
-    exit 1
-fi
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
