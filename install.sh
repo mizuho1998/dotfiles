f7188db03a35ef9c3f808dff676310b674d01cdd
mizuho1998 <moc.liam0000@gmail.com>
2021-04-05 21:49:05 +0900

    fix: install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
         read line </etc/os-release && eval ${line}
         case ${NAME} in
         "CentOS Linux") ;;
-        "Ubuntu") ;;
+        "Ubuntu")
             ./ubuntu/install.sh
+            ;;
         *) ;;
         esac
     fi
@@ -21,8 +22,8 @@ else
 fi
 
 # install zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
