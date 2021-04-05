2dadafa65f3b0a962a175b8a03a35401cc5fa865
mizuho1998 <moc.liam0000@gmail.com>
2021-04-05 21:50:50 +0900

    feat: remove commentout

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ else
 fi
 
 # install zplug
-# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # install vim-plug
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
