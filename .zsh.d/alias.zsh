c7b2c5d8e1417d90d9bc432ae8eee22c4629c128
mizuho1998 <wataru_fujii@voyagegroup.com>
2021-04-06 14:26:42 +0900

    feat: alias

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -1,1 +1,14 @@
-
+case "${OSTYPE}" in
+darwin*)
+    alias ls="ls -G"
+    alias ll="ls -lG"
+    alias la="ls -laG"
+    ;;
+linux*)
+    alias ls='ls --color=auto'
+    alias la='ls -a --color'
+    alias ll='ls -l --color'
+    alias lla='ls -al --color'
+    alias open='nautilus'
+    ;;
+esac
