d1e84eef7d90220a39a0fbcf83745ac2b78d80b2
mizuho1998 <wataru_fujii@voyagegroup.com>
2021-04-07 10:16:03 +0900

    fix: ln vscode snippets

--- vscode_settings/deploy.sh
+++ vscode_settings/deploy.sh
@@ -4,12 +4,12 @@ if [ "$(uname)" == 'Darwin' ]; then
     # OS='Mac'
     ln -s `pwd`"/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
     ln -s `pwd`"/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
-    ln -s `pwd`"/snippets" "$HOME/Library/Application Support/Code/User/snippets"
+    ln -s `pwd`"/snippets/" "$HOME/Library/Application Support/Code/User/snippets"
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     # OS='Linux'
     ln -s `pwd`"/settings.json" "$HOME/.config/Code/User/settings.json"
     ln -s `pwd`"/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
-    ln -s `pwd`"/snippets" "$HOME/.config/Code/User/snippets"
+    ln -s `pwd`"/snippets/" "$HOME/.config/Code/User/snippets"
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
