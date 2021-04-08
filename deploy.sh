49580830e5480929b88ef5d6d4e6dfb5253a57c3
mizuho1998 <wataru_fujii@voyagegroup.com>
2021-04-08 19:14:46 +0900

    feat: add settings process in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ "$(uname)" == 'Darwin' ]; then
+    echo 'Mac'
+    ./mac/settings.sh --term-path ./mac
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    echo 'Linux'
+    # Linuxディストリビューションごとの設定
+    if [ -f "/etc/os-release" ]; then
+        read line </etc/os-release && eval ${line}
+        case ${NAME} in
+        "CentOS Linux") ;;
+        "Ubuntu") ;;
+        *) ;;
+        esac
+    fi
+fi
+
+
 # ドットファイルのシンボリックリンクの作成
 for f in .??*
 do
