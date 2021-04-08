fe0a2f1a779ccce2afee550b14910e54bfcc20ca
mizuho1998 <wataru_fujii@voyagegroup.com>
2021-04-08 18:16:11 +0900

    feat: add mac settings

---
+++ mac/settings.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# dock
+defaults write com.apple.dock autohide -int 0
+defaults write com.apple.dock magnification -int 0
+defaults write com.apple.dock mineffect -string scale
+defaults write com.apple.dock "minimize-to-application" -int 1
+defaults write com.apple.dock orientation -string left
+defaults write com.apple.dock "show-process-indicators" -int 1
+defaults write com.apple.dock "show-recents" -int 0
+defaults write com.apple.dock tilesize -int 40
+
+# keyboard
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+# mouse
+
+# trackpad
+defaults write NSGlobalDomain "com.apple.trackpad.scaling" -int 3
+defaults write "com.apple.AppleMultitouchTrackpad" FirstClickThreshold -int 0
+defaults write "com.apple.AppleMultitouchTrackpad" SecondClickThreshold -int 0
+
+# bluetooth
+defaults write  "com.apple.systemuiserver" "NSStatusItem Visible com.apple.menuextra.bluetooth" -int 1
+
+# battery
+defaults write "com.apple.menuextra.battery" ShowPercent -string "YES"
+
+# clock
+defaults write "com.apple.menuextra.clock" DateFormat -string "M\\U6708d\\U65e5(EEE)  H:mm";
+defaults write "com.apple.menuextra.clock" FlashDateSeparators -int 0
+defaults write  "com.apple.menuextra.clock" IsAnalog -int 0
+
+# menu extra
+defaults write "com.apple.systemuiserver" "NSStatusItem Visible com.apple.menuextra.clock" -int 1
+defaults write "com.apple.systemuiserver" menuExtras -array \
+"/System/Library/CoreServices/Menu Extras/Clock.menu" \
+"/System/Library/CoreServices/Menu Extras/Battery.menu" \
+"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+"/System/Library/CoreServices/Menu Extras/Displays.menu" \
+"/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
+
+# finder
+defaults write "com.apple.finder" NewWindowTarget -string "PfHm"
+defaults write "com.apple.finder" NewWindowTargetPath -string "file://${HOME}/"
+defaults write "com.apple.finder" ShowExternalHardDrivesOnDesktop -int 1
+defaults write "com.apple.finder" ShowHardDrivesOnDesktop -int 1
+defaults write "com.apple.finder" ShowMountedServersOnDesktop -int 1
+defaults write "com.apple.finder" ShowRemovableMediaOnDesktop -int 1
+defaults write NSGlobalDomain AppleShowAllExtensions -int 1
+defaults write NSGlobalDomain FXEnableExtensionChangeWarning -int 1
+
+# terminal
+TERM_PROFILE='custom';
+TERM_PATH='./';
+CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
+if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
+    # open "$TERM_PATH$TERM_PROFILE.terminal"
+    defaults write com.apple.Terminal "Default Window Settings" -string "$TERM_PROFILE"
+    defaults write com.apple.Terminal "Startup Window Settings" -string "$TERM_PROFILE"
+fi
+# defaults import com.apple.Terminal "$HOME/Library/Preferences/com.apple.Terminal.plist"
