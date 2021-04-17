12e204c01a23f607a3c6f71d6ca2cebe28e12614
mizuho1998 <moc.liam0000@gmail.com>
2021-04-17 11:35:41 +0900

    feat: add gh installation on ubuntu

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -19,6 +19,14 @@ for formula in "${formulas[@]}"; do
     sudo apt install -y $formula
 done
 
+# install gh
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+# sudo apt-get install dirmngr 
+sudo apt install gh
+
 # install zshrc
 chsh -s $(which zsh)
 
